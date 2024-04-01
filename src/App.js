4eb4983db0bf9ea991103cc27430c377c63f17f6
abbiedesignco <abbier.design@gmail.com>
2024-04-01 11:57:54 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.abbiericher.com/" target="_blank">
+          Matt Delac
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/abbiedesignco/react-weather-abbie-richer"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
